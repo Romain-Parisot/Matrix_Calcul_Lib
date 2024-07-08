0cc731f3d1cc348dd42d7bc9360b2b03e6c1d58f
admsmn02 <adam.simon@edu.devinci.fr>
2024-07-08 16:27:27 +0200

    feat: added Hadamard to index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { transpose } from "./src/transpose.js";
 import { subtract } from "./src/subtract.js";
 import { multiply } from "./src/multiply.js";
 import { inverse } from "./src/inverse.js";
+import { hadamard } from "./src/hadamard.js";
 
 let matrix1 = new Matrix([
   [1, 2],
@@ -18,40 +19,43 @@ let matrix3 = new Matrix([
   [4, 5, 6],
   [7, 8, 9],
 ]);
-let transposed = transpose(matrix3.data);
-console.log(transposed);
 
 let matrix4 = new Matrix([
-    [10, 11],
-    [12, 13],
+  [10, 11],
+  [12, 13],
 ]);
 let matrix5 = new Matrix([
-    [1, 2],
-    [3, 4],
+  [1, 2],
+  [3, 4],
 ]);
+
 let subtractionResult = subtract(matrix4, matrix5);
 console.log("Subtraction Result:");
 console.log(subtractionResult.data);
 
-let result = add(matrix1.data, matrix2.data);
+let additionResult = add(matrix1.data, matrix2.data);
 console.log("Addition Result:");
-console.log(result);
+console.log(additionResult);
 
-let multiply_result = multiply(matrix1, matrix2);
+let multiplicationResult = multiply(matrix1, matrix2);
 console.log("Multiplication Result:");
-console.log(multiply_result.data);
+console.log(multiplicationResult.data);
 
-let transposed_result = transpose(matrix3.data);
+let transposedResult = transpose(matrix3.data);
 console.log("Transpose Result:");
-console.log(transposed_result);
+console.log(transposedResult);
+
+let hadamardResult = hadamard(matrix1.data, matrix2.data);
+console.log("Hadamard Result:");
+console.log(hadamardResult);
 
 try {
-  let inverse_result = inverse([
+  let inverseResult = inverse([
     [1, 2],
     [3, 4],
   ]);
   console.log("Inverse Result:");
-  console.log(inverse_result);
+  console.log(inverseResult);
 } catch (error) {
   console.error("Error computing inverse:", error.message);
 }
