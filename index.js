d315857233e32dfca8e784bda287d95bbcde1041
admsmn02 <adam.simon@edu.devinci.fr>
2024-07-08 15:17:17 +0200

    feat: added transpose to index

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-import Matrix  from './matrix.js';
-import { add } from './src/add.js';
+import Matrix from "./matrix.js";
+import { add } from "./src/add.js";
+import { transpose } from "./src/transpose.js";
 
-let matrix1 = new Matrix([[1, 2], [3, 4]]);
-let matrix2 = new Matrix([[5, 6], [7, 8]]);
+let matrix1 = new Matrix([
+  [1, 2],
+  [3, 4],
+]);
+let matrix2 = new Matrix([
+  [5, 6],
+  [7, 8],
+]);
 let result = add(matrix1.data, matrix2.data);
 console.log(result);
+
+let matrix3 = new Matrix([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
+let transposed = transpose(matrix3.data);
+console.log(transposed);
