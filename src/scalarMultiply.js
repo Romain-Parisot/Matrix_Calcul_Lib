53b69ee2c5f8f0795ada1bed50e3f4377abb1fd2
unknown <romainparisot.pro@gmail.com>
2024-07-08 15:30:05 +0200

    docs: add jsdoc

--- src/scalarMultiply.js
+++ src/scalarMultiply.js
@@ -1,3 +1,10 @@
+/**
+
+Multiplies a matrix by a scalar.*
+@param {number[][]} matrix - The matrix to be multiplied.
+@param {number} scalar - The scalar value.
+@returns {number[][]} The resulting matrix after multiplication.
+*/
 export function scalarMultiply(matrix, scalar) {
   return matrix.map((row) => row.map((value) => (value *= scalar)));
 }
