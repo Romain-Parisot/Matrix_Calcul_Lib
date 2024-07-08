4ae840c6add89f439494837e581b1debee54c5d7
unknown <romainparisot.pro@gmail.com>
2024-07-08 15:17:30 +0200

    feat: add multiply in index

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import Matrix  from './matrix.js';
-import { add } from './src/add.js';
+import Matrix from "./matrix.js";
+import { add } from "./src/add.js";
+import { multiply } from "./src/multiply.js";
 
-let matrix1 = new Matrix([[1, 2], [3, 4]]);
-let matrix2 = new Matrix([[5, 6], [7, 8]]);
+let matrix1 = new Matrix([
+  [1, 2],
+  [3, 4],
+]);
+let matrix2 = new Matrix([
+  [5, 6],
+  [7, 8],
+]);
 let result = add(matrix1.data, matrix2.data);
+
+let multiply_result = multiply(matrix1, matrix2);
 console.log(result);
