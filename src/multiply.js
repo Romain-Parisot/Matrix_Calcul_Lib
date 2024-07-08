4d7257dbfaef4901679cf2cd1dd28049270c08e0
unknown <romainparisot.pro@gmail.com>
2024-07-08 15:14:07 +0200

    docs: add jsdoc

--- src/multiply.js
+++ src/multiply.js
@@ -1,5 +1,14 @@
 import Matrix from "./Matrix.js";
 
+/**
+ * Multiplies two matrices.
+ *
+ * @param {Matrix} matrix1 - The first matrix to multiply.
+ * @param {Matrix} matrix2 - The second matrix to multiply.
+ * @returns {Matrix} The resulting matrix after multiplication.
+ *
+ * @throws {Error} If the number of columns in the first matrix does not equal the number of rows in the second matrix.
+ */
 export function multiply(matrix1, matrix2) {
   let result = matrix1.data.map((row, i) =>
     Array(matrix2.data[0].length)
